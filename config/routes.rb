70c0ea1bf81a9d646bb702634e558a7d12624154
suzukinan <tateos.study.only@gmail.com>
2019-04-21 16:27:56 +0900

    メッセージ機能の実装
    メッセージ送信機能の実装
    メッセージにバリデーションをかける
    ビューの修正
    フラッシュメッセージの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root 'messages#index'
-  root 'groups#index'
-  resources :users, only: [:index, :edit, :update]
+  root 'messages#index'
+  # root 'groups#index'
+  resource :user, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
