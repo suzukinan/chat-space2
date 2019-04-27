98528b3dab0491ef8e1e564d17adeae1fd37a454
suzukinan <tateos.study.only@gmail.com>
2019-04-27 20:50:41 +0900

    インクリメンタルサーチ
    app/assets/javascripts/search.js
    ６行目
    function appendUser(user)から
    function appendUserName(user) に変更
    
    41行目
    appendUser(user); から
    appendUserName(user);へ変更

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,7 +3,7 @@ $(function() {
 
     var search_list = $("#user-search-result");
 
-    function appendUser(user) {
+    function appendUserName(user) {
       var html = `<div class='chat-group-user clearfix'>
                   <p class='chat-group-user__name'>${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -38,7 +38,7 @@ $(function() {
         $("#user-search-result").empty();
         if (users.length !==0 ) {
           users.forEach(function(user){
-            appendUser(user);
+            appendUserName(user);
           });
         }
         else {
