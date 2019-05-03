2e09da2326d59e3df516748aff71f095b4a64c57
suzukinan <tateos.study.only@gmail.com>
2019-05-03 15:33:11 +0900

    Deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock '~> 3.11.0'
 
-set :application, "chat-space2"
-set :repo_url, "git@example.com:suzukinan/chat-space2.git"
+set :application, 'chat-space2'
+set :repo_url, 'git@github.com:suzukinan/chat-space2.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
